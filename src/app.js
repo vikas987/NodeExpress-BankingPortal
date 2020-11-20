fdf8c8657d331552f4645cd99491b63af5e007e9
Vikas Shivaraj <M1046867@mindtree.com>
2020-11-21 03:24:45 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const { accounts, users, writeJSON } = require('./data');
+const { accounts, users } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts: accounts });
@@ -15,43 +18,6 @@ app.get('/', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] })
-})
-
-app.get('/savings', (req, res) => {
-    res.render('account', { account: accounts.savings });
-})
-
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking });
-})
-
-app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit });
-})
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer')
-})
-
-app.post('/transfer', (req, res) => {
-    const from = req.body.from;
-    const to = req.body.to;
-    const amount = req.body.amount;
-    accounts[from].balance -= amount;
-    accounts[to].balance += parseInt(amount);
-    writeJSON();
-    res.render('transfer', { message: "Transfer Completed" })
-})
-
-app.get('/payment', (req, res) => {
-    res.render('payment', { account: accounts.credit })
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', { message: "Payment Successful", account: accounts.credit })
 })
 
 app.listen(3000, () => {
---
+++ src/routes/services.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { accounts, writeJSON } = require('../data');
+
+router.get('/transfer', (req, res) => {
+    res.render('transfer')
+})
+
+router.post('/transfer', (req, res) => {
+    const from = req.body.from;
+    const to = req.body.to;
+    const amount = req.body.amount;
+    accounts[from].balance -= amount;
+    accounts[to].balance += parseInt(amount);
+    writeJSON();
+    res.render('transfer', { message: "Transfer Completed" })
+})
+
+router.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit })
+})
+
+router.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    writeJSON();
+    res.render('payment', { message: "Payment Successful", account: accounts.credit })
+})
+
+module.exports = router;
