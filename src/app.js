0806d883163d567c922cd6d80689c71118bb2e5b
Vikas Shivaraj <M1046867@mindtree.com>
2020-11-21 02:17:52 +0530

    4

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
 
 const accountData = fs.readFileSync(__dirname + '/json/accounts.json', { encoding: 'UTF8' });
 const accounts = JSON.parse(accountData);
@@ -32,6 +33,12 @@ app.get('/checking', (req, res) => {
 app.get('/credit', (req, res) => {
     res.render('account', { account: accounts.credit });
 })
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer')
+})
+
+app.post('/transfer', (req, res) => { })
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!')
