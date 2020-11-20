aadff3d80cfca1935b885ae10c55bf94dd4b3739
Vikas Shivaraj <M1046867@mindtree.com>
2020-11-21 01:52:06 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -7,8 +7,30 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(__dirname + '/json/accounts.json', { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(__dirname + '/json/users.json', { encoding: 'UTF8' })
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Index' });
+    res.render('index', { title: 'Account Summary', accounts: accounts });
+})
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] })
+})
+
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+})
+
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+})
+
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
 })
 
 app.listen(3000, () => {
