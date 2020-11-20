474d4ee969ae29a23bfd99c80ed6dedbff3f93d6
Vikas Shivaraj <M1046867@mindtree.com>
2020-11-21 02:54:23 +0530

    10

--- src/app.js
+++ src/app.js
@@ -49,6 +49,20 @@ app.post('/transfer', (req, res) => {
     res.render('transfer', { message: "Transfer Completed" })
 })
 
+
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit })
+})
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit })
+})
+
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!')
 })
