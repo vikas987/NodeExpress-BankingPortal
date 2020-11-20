41bae4ebb2951bd7542b11e9e39fba42bed32903
Vikas Shivaraj <M1046867@mindtree.com>
2020-11-21 01:16:43 +0530

    Completed Module

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { title: 'Index' });
+    res.render('index', { title: 'Index' });
 })
 
 app.listen(3000, () => {
